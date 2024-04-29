676bcf617a30e2b1bb4088df59836670cd544662
rahulroyrdx001 <rahulroyrdx001@gmail.com>
2024-04-29 21:40:58 +0530

    Minimum changes

--- Tuneify/src/components/LandingPage.jsx
+++ Tuneify/src/components/LandingPage.jsx
@@ -1,4 +1,3 @@
-
 import { useTypewriter, Cursor } from "react-simple-typewriter";
 import style from "../style components/LandingPage.module.css";
 import VideoPage from "../assets/Video.mp4";
@@ -11,23 +10,22 @@ function LandingPage() {
   });
   return (
     <>
-      <div>
-    <div className={style.videoPage}>
-      <video
-        className={style.video}
-        src={VideoPage}
-        autoPlay
-        loop
-        muted
-      ></video>
-      <h1 style={{ margin: "50px", fontSize: "70px" }}>
+      <div className={style.videoPage}>
+        <video
+          className={style.video}
+          src={VideoPage}
+          autoPlay
+          loop
+          muted
+        ></video>
+        <h1 style={{marginTop:"0px",  margin: "0px", padding: "50px", fontSize: "70px" }}>
           {" "}
           <span style={{ fontWeight: "bold", color: "#1DDA63" }}>{text}</span>
           <span style={{ color: "red" }}>
             <Cursor cursorStyle="|"></Cursor>
           </span>
         </h1>
-    </div>
+      </div>
     </>
   );
 }
