87687518707ccd26c92da4a9e46b898318cfe9e0
AnupriyaKumarii <anuyadav20001@gmail.com>
2024-04-29 20:31:59 +0530

    LandingPage

--- Tuneify/src/App.jsx
+++ Tuneify/src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css'
+import "./App.css";
+import LandingPage from "./components/LandingPage";
 
 function App() {
-
   return (
     <>
-      
+      <LandingPage></LandingPage>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
