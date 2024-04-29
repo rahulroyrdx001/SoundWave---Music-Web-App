a1695171d09d0170d52bbdb2e1013bd9608ffdc4
rahulroyrdx001 <rahulroyrdx001@gmail.com>
2024-04-29 11:58:36 +0530

    Initialize the typing effect

--- Tuneify/src/App.jsx
+++ Tuneify/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import LandingPage from './components/LandingPage'
 
 function App() {
 
   return (
     <>
-      
+      <LandingPage></LandingPage>
     </>
   )
 }
--- Tuneify/src/components/LandingPage.jsx
+++ Tuneify/src/components/LandingPage.jsx
@@ -1,5 +1,24 @@
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 function LandingPage() {
-  return <div></div>;
+  const [text] = useTypewriter({
+    words: ["Welcome to Tuneify !", "Unlimited music", "Anytime", "Anywhere"],
+    loop: {},
+    typeSpeed: 100,
+    deleteSpeed: 100,
+  });
+  return (
+    <>
+      <div>
+        <h1 style={{ margin: "50px", fontSize: "70px" }}>
+          {" "}
+          <span style={{ fontWeight: "bold", color: "#1DDA63" }}>{text}</span>
+          <span style={{ color: "red" }}>
+            <Cursor cursorStyle="|"></Cursor>
+          </span>
+        </h1>
+      </div>
+    </>
+  );
 }
 
 export default LandingPage;
