64a1a7a1600de7b14f9a2a41836e3eb33f858250
rahulroyrdx001 <rahulroyrdx001@gmail.com>
2024-05-08 21:57:02 +0530

    LOGO ADDED

--- Tuneify/src/components/LandingPage.jsx
+++ Tuneify/src/components/LandingPage.jsx
@@ -1,9 +1,11 @@
 import { useTypewriter, Cursor } from "react-simple-typewriter";
 import style from "../style components/LandingPage.module.css";
 import VideoPage from "../assets/Video.mp4";
+import logo1 from "/resourses/logo1.png";
+
 function LandingPage() {
   const [text] = useTypewriter({
-    words: ["Welcome to Tuneify !", "Unlimited music", "Anytime", "Anywhere"],
+    words: ["Welcome to Tuneify!", "Unlimited music", "Anytime Anywhere"],
     loop: {},
     typeSpeed: 100,
     deleteSpeed: 100,
@@ -18,13 +20,26 @@ function LandingPage() {
           loop
           muted
         ></video>
-        <h1 style={{marginTop:"0px",  margin: "0px", padding: "50px", fontSize: "70px" }}>
-          {" "}
-          <span style={{ fontWeight: "bold", color: "#1DDA63" }}>{text}</span>
-          <span style={{ color: "red" }}>
-            <Cursor cursorStyle="|"></Cursor>
-          </span>
-        </h1>
+        <div className={style.mytext}>
+          <h1
+            style={{
+              marginTop: "0px",
+              margin: "0px",
+              padding: "0px",
+              paddingLeft: "100px",
+              paddingTop: "100px",
+              fontSize: "135px",
+              width: "100px",
+            }}
+          >
+            {" "}
+            <span style={{ fontWeight: "bold", color: "white" }}>{text}</span>
+            <span style={{ color: "#1DD962" }}>
+              <Cursor cursorStyle="|"></Cursor>
+            </span>
+          </h1>
+        </div>
+        <img className={style.mylogo} src={logo1}></img>
       </div>
     </>
   );
