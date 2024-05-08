675acfe3190de96d0415d30523b33470648f1602
AnupriyaKumarii <anuyadav20001@gmail.com>
2024-05-08 22:06:59 +0530

    Heading

--- Tuneify/src/components/LandingPage.jsx
+++ Tuneify/src/components/LandingPage.jsx
@@ -1,6 +1,7 @@
 import { useTypewriter, Cursor } from "react-simple-typewriter";
 import style from "../style components/LandingPage.module.css";
 import VideoPage from "../assets/Video.mp4";
+
 function LandingPage() {
   const [text] = useTypewriter({
     words: ["Welcome to Tuneify !", "Unlimited music", "Anytime", "Anywhere"],
@@ -18,7 +19,15 @@ function LandingPage() {
           loop
           muted
         ></video>
-        <h1 style={{marginTop:"0px",  margin: "0px", padding: "50px", fontSize: "70px" }}>
+        <h1
+          className={style.heading}
+          style={{
+            marginTop: "0px",
+            margin: "0px",
+            padding: "50px",
+            fontSize: "110px",
+          }}
+        >
           {" "}
           <span style={{ fontWeight: "bold", color: "#1DDA63" }}>{text}</span>
           <span style={{ color: "red" }}>
